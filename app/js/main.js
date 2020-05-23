311fdde6a59d2b00583b167052b2c43a48e3ad72
temirland <rakanov.t@gmail.com>
2020-05-23 12:53:34 +0600

    added organizations page

--- app/js/main.js
+++ app/js/main.js
@@ -13,7 +13,14 @@ $(function(){
 
 
 
-
+    $('.organizations__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+        $('.organizations__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.organizations__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+    });
 
 
 
